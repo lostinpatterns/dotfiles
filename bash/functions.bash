018b460645a74c1961215344fdbf3ce00643fe2b
lostinpatterns <blweiner@gmail.com>
2012-11-14 13:47:38 -0800

    added in static server function

--- bash/functions.bash
+++ bash/functions.bash
@@ -31,3 +31,12 @@ function fs() {
     du $arg .[^.]* *
   fi
 }
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+  local port="${1:-8000}"
+  open "http://localhost:${port}/"
+  # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+  # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+  python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
