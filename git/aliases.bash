f71b54d4772558cea96a71dc0d1e4c11dfda13aa
lostinpatterns <blweiner@gmail.com>
2013-06-19 12:46:17 -0700

    updates

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,4 +1,7 @@
-alias git-commit='git commit -m "$1"'
-alias git-destage='git reset HEAD '
+# some taken from: http://durdn.com/blog/2012/11/22/must-have-git-aliases-advanced-examples/
+
+alias git-commit='git commit -m '
+alias git-unstage='git reset HEAD '
+alias git-status='git status -s '
 
 alias github-clone='cd $PROJECTS/github && git clone '
--- sublime/load_preferences.sh
+++ sublime/load_preferences.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: ideally we'd symlink rather than copy, but sublime doesn't seem to support that
+
 source_path=$DOTFILES/sublime/User
 target_path=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 
